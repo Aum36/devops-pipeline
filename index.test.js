f9661a176720a41c2d7724779ff9caa271560efa
Aum Kulkarni <aumwinay@gmail.com>
2024-03-16 14:17:06 +0530

    Modified test and Edited Counter.jsx for ease of use

--- index.test.js
+++ index.test.js
@@ -9,7 +9,9 @@ let driver
 const rootURL = process.env.WEB_URL
 console.log(rootURL);
 beforeAll(async () => {
-     driver = await new Builder().forBrowser(Browser.CHROME).setChromeOptions(options.addArguments('--headless=new')).build();
+     driver = await new Builder().forBrowser(Browser.CHROME)
+    //  .setChromeOptions(options.addArguments('--headless=new'))
+     .build();
     console.log("Before All")
 })
 
@@ -24,17 +26,21 @@ it('initializes the context', async () => {
 
 describe("testing counter page", () => {
     it("opening counter page", async () => {
-        let counterPage = await driver.findElement(By.xpath('//*[@id="root"]/section/div[1]/div/div[2]'));
+        let counterPage = await driver.findElement(By.xpath('//*[@id="root"]/section/div[1]/div/div[3]'));
         await counterPage.click();
     })
 
-    it("testing counter button", async () => {
+    it("clicking counter button", async () => {
         await driver.wait(until.elementLocated(By.xpath('//*[@id="root"]/section/div[2]/button')), 8000);
         let counterButton = await driver.findElement(By.xpath('//*[@id="root"]/section/div[2]/button'));
-        await counterButton.click();
+        let counterText = await driver.findElement(By.xpath('//*[@id="root"]/section/div[2]'));
+        let text = await counterText.getText()
+        console.log(text.split(" "));
+        let i = 0
+        while (i != 7) {
+            await counterButton.click();
+            i = i + 1;
+        }
         console.log("After All");
     })
-})
-it('should click on navbar button to display a drawer', async () => {
-    console.log("Second it")
 })
